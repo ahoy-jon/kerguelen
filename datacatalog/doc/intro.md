ca70130d3b1ddccd1fb02fcb3380fe5cc078ad2d
Jonathan WINANDY <jonathan.winandy@gmail.com>
2014-01-17 14:53:41 +0100

    init

---
+++ datacatalog/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to datacatalog
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ datacatalog/test/datacatalog/core_test.clj
@@ -0,0 +1,7 @@
+(ns datacatalog.core-test
+  (:require [clojure.test :refer :all]
+            [datacatalog.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
